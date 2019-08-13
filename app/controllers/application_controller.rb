2324e67c0dd8ff85348c52dc11f0395763482754
olesya ogorodnikova <ooleszja@gmail.com>
2019-08-13 22:30:48 +0200

    rails new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def index
-    @tasks = Task.all
-  end
-
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/tasks", to: 'tasks#index'
-  # # Read one restaurant
-  get "/tasks/:id", to: 'tasks#show'
-  # # Create new restaurant
-  get "/tasks/new", to: 'tasks#new'
-  post "/rtasks", to: 'tasks#create'
-  # # Update restaurant
-  get "/tasks/:id/edit", to: 'tasks#edit'
-  patch "/rtasks/:id", to: 'tasks#update'
-  # # Delete restaurant
-  delete "/tasks/:id", to: 'tasks#destroy'
-
 end
